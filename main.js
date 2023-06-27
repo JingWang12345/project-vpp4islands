9739821d3091cee7070ba3a795ca7a1b6caddba1
JingWang <wangjing061993@gmail.com>
2023-06-27 10:46:39 +0200

    update popup

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function showPopup() {
     let overlay = document.getElementById('overlay');
     overlay.style.display = "block";
 }
+function hidePopup() {
+    let overlay = document.getElementById('overlay');
+    overlay.style.display = "none";
+}
